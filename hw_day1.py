dfca50f780c7b151bf8310b17215235a9f0c1f63
jgarrido96 <jgarrido96@gmail.com>
2024-03-19 01:53:32 -0400

    Homework 3.18.24

---
+++ hw_day1.py
@@ -0,0 +1,86 @@
+# 1. Shocase Your Dance Moves!
+
+# Task 1: Code Correction
+
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+print("\n")
+
+# Task 2: Your Mood Today
+      
+user_input = input('How do you feel today? ')
+if user_input == "Happy":
+    print("That's great to hear!")
+if user_input == "Sad":
+    print("I hope your day gets better!")
+
+print("\n \n")
+
+# 2. Python Naming Convention Practice
+
+# Task 1: Code Correction
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+max_limit = 1000
+
+print("\n \n")
+# 3. Python Data Types and type() Function
+
+# Task 1: Code Correction
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+print("\n \n")
+
+# 4. Arithmetic Operations in Daily Life
+
+# Task 1: Grocery Store Math 
+
+rice_price = 5
+chicken_price = 12
+cooking_oil_price = 7
+
+total_bill = rice_price + chicken_price + cooking_oil_price
+
+print(total_bill)
+
+# Task 2: Bank Interest
+
+money_in_account = 4500
+interest_rate = 1.04
+time = 1
+
+total_interest = money_in_account * interest_rate * time
+
+print(total_interest)
+
+print("\n \n")
+
+# 5. Understanding Assignments and Comparison
+
+# Task 1: Value Swapping
+
+num_1 = 7
+num_2 = 11
+print(num_1)
+print(num_2)
+
+num_1 = 11
+num_2 = 7
+print(num_1)
+print(num_2)
+
+# Hope you have a good day!
